c790bbe5ee1b815bc89da435033cf4862ee30088
valentin di domenica <valendido69@gmail.com>
2023-10-16 17:12:01 -0300

    hw sequelize 1 done

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,15 @@
 const server=require('./app')
+const {conn}=require('./DB_connection')
 
 const PORT = 3001;
 
-server.listen(PORT, () => {
-  console.log(`Server running into ${PORT} Port`);
-});
+
+conn.sync({ force: true })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server running into ${PORT} Port`);
+    });
+  })
+  .catch((error) => {
+    console.error('No se pudo iniciar el servidor:', error);
+  });
